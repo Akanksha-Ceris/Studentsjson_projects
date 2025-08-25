df09f8501d5345bbf21eb63e5b75f643df0b15a0
Your name <akankshaceris@gmail.comexit()clearexit>
2025-08-25 21:02:47 +0530

    your commit message

--- Studentsm.py
+++ Studentsm.py
@@ -1,14 +1,14 @@
-import json
-student={}
-student["enrollment_no"] = input("Enter Enrollment Number:")
-student["name"] =input("Enter Name:")
-student["program"] = input("Enter Your programm:")
-student["Session"] =input("Enter Session:")
+# import json
+# student={}
+# student["enrollment_no"] = input("Enter Enrollment Number:")
+# student["name"] =input("Enter Name:")
+# student["program"] = input("Enter Your programm:")
+# student["Session"] =input("Enter Session:")
 
-student_json = json.dumps(student,indent=4)
+# student_json = json.dumps(student,indent=4)
 
 
-print("\nStudent Details in JSON format:")
-print(student_json)
-with open("student.json","w") as f:
-    f.write(student_json)
+# print("\nStudent Details in JSON format:")
+# print(student_json)
+# with open("student.json","w") as f:
+#     f.write(student_json)
--- studentpdf.py
+++ studentpdf.py
@@ -1,10 +1,10 @@
-import fitz 
-import json
-doc =fitz.open("json pdf.pdf")
-text = ""
-for page in doc:
-    text += page.get_text()
-data = {"put_text": text}
-with open("student.json","w") as f:
-    json.dump(data,f,indent=4)
-    print("pdf text saved into student.json")    
+# import fitz 
+# import json
+# doc =fitz.open("json pdf.pdf")
+# text = ""
+# for page in doc:
+#     text += page.get_text()
+# data = {"put_text": text}
+# with open("student.json","w") as f:
+#     json.dump(data,f,indent=4)
+#     print("pdf text saved into student.json")    
